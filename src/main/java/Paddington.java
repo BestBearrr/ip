a39f87e20585b623d10f648912eba980b2175158
LAPTOP-KHJ8QPQL\archw <luohongxun3@gmail.com>
2025-08-27 14:55:33 +0800

    Renamed Chatbot

---
+++ src/main/java/Paddington.java
@@ -0,0 +1,17 @@
+public class Paddington {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);*/
+        String line = "____________________________________________________________\n";
+        System.out.print(line);
+        System.out.println("Hello! I'm Paddington");
+        System.out.println("What can I do for you?");
+        System.out.print(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print(line);
+    }
+}
