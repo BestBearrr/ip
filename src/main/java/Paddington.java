20efb6f8955c59150e0a6627f2833cc122621c22
BestBearrr <luohongxun3@gmail.com>
2025-09-01 19:14:29 +0800

    Add Tasks, List

--- src/main/java/Paddington.java
+++ src/main/java/Paddington.java
@@ -14,23 +14,43 @@ public class Paddington {
         System.out.println("What can I do for you?");
         System.out.print(line);
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.print(line);
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
 
         Scanner scanner = new Scanner(System.in);
         String userInput;
 
         while (true) {
-            userInput = scanner.nextLine();
+            userInput = scanner.nextLine().toLowerCase();
+
+            System.out.print(line);
 
             if (userInput.equals("bye")) {
-                System.out.println(line);
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(line);
+                System.out.print(line);
                 break;
+            } else if (userInput.equals("list")) {
+                if (taskCount == 0) {
+                    System.out.println("No saved tasks.");
+                } else {
+                    // Display all saved tasks
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i].getDescription());
+                    }
+                }
             }
+            // Add new task
+            else {
+                Task t = new Task(userInput);
+                tasks[taskCount] = t;
+                taskCount++;
+
+                System.out.println("added: " + userInput);
+            }
+
+            System.out.print(line);
         }
 
-
+        scanner.close();
     }
 }
