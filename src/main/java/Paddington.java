69c5afd840742bf014c15da202ba5a3d6ff6af7b
BestBearrr <luohongxun3@gmail.com>
2025-09-14 13:07:24 +0800

    Restructure code into functions to improve readability and reduce duplication

--- src/main/java/Paddington.java
+++ src/main/java/Paddington.java
@@ -1,76 +1,99 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-
-
 public class Paddington {
+    static final String line = "____________________________________________________________\n";
+    static final int MAX_TASK_SIZE = 100;
 
-    static final int TASK_SIZE = 100;
+    static ArrayList<Task> tasksList = new ArrayList<>(MAX_TASK_SIZE);
+    static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    private static void printWelcomeMessage() {
         /*String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);*/
-        String line = "____________________________________________________________\n";
+
         System.out.print(line);
         System.out.println("Hello! I'm Paddington");
         System.out.println("What can I do for you?");
         System.out.print(line);
+    }
 
-        Task[] tasks = new Task[TASK_SIZE];
-        int taskCount = 0;
+    private static void printTask(int index) {
+        System.out.println(tasksList.get(index).toString());
+    }
 
-        Scanner scanner = new Scanner(System.in);
-        String userInput;
+    private static void listAllTasks() {
+        // Display all saved tasks
+        if (tasksList.isEmpty()) {
+            System.out.println("No saved tasks.");
+            return;
+        }
 
-        while (true) {
-            userInput = scanner.nextLine().toLowerCase();
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < tasksList.size(); i++) {
+            System.out.print((i + 1) + ".");
+            printTask(i);
+        }
+    }
 
-            System.out.print(line);
+    private static void markTask(String input) {
+        int taskIndex = Integer.parseInt(input) - 1;
+        Task task = tasksList.get(taskIndex);
+        task.markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+    }
 
-            if (userInput.equals("bye")) {
+    private static void unmarkTask(String input) {
+        int taskIndex = Integer.parseInt(input) - 1;
+        Task task = tasksList.get(taskIndex);
+        task.unmarkAsDone();
+        System.out.println("OK, I've marked this task as not done yet");
+        System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+    }
+
+    private static boolean userCommands(String userInput) {
+        String[] processedInput = userInput.split(" ", 2);
+        String command = processedInput[0];
+        String input = (processedInput.length > 1) ? processedInput[1] : "";
+
+        switch(command) {
+            case "bye":
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.print(line);
+                return true;
+            case "list":
+                listAllTasks();
                 break;
-            } else if (userInput.equals("list")) {
-                if (taskCount == 0) {
-                    System.out.println("No saved tasks.");
-                } else {
-                    // Display all saved tasks
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] "
-                                + tasks[i].getDescription());
-                    }
-                }
-            }
-            // Mark as done
-            else if (userInput.startsWith("mark ")) {
-                int taskIndex = Integer.parseInt(userInput.substring(5)) - 1;
-                Task t = tasks[taskIndex];
-                System.out.println("Nice! I've marked this task as done:");
-                t.markAsDone();
-                System.out.println("  [" + t.getStatusIcon() + "] " + t.description);
-            }
-            // Unmark as done
-            else if (userInput.startsWith("unmark ")) {
-                int taskIndex = Integer.parseInt(userInput.substring(7)) - 1;
-                Task t = tasks[taskIndex];
-                System.out.println("OK, I've marked this task as not done yet");
-                t.unmarkAsDone();
-                System.out.println("  [" + t.getStatusIcon() + "] " + t.description);
-            }
-            // Add new task
-            else {
-                Task t = new Task(userInput);
-                tasks[taskCount] = t;
-                taskCount++;
-
+            case "mark":
+                markTask(input);
+                break;
+            case "unmark":
+                unmarkTask(input);
+                break;
+            default:
+                // No command given, so add new task.
+                Task task = new Task(userInput);
+                tasksList.add(tasksList.size(), task);
                 System.out.println("added: " + userInput);
-            }
+        }
 
+        System.out.println(line);
+        return false; // Continue
+    }
+
+    public static void main(String[] args) {
+        printWelcomeMessage();
+        while (true) {
+            String userInput = scanner.nextLine().toLowerCase();
             System.out.print(line);
+
+            // True: quit; False: continue
+            if (userCommands(userInput)) break;
         }
 
         scanner.close();
